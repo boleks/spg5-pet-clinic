2488d21e35a5971b6ef4c7af98e129724a0c2b17
boleks <51618150+boleks@users.noreply.github.com>
2019-10-18 14:33:46 +0200

    initial project

---
+++ src/main/java/com/boleks/spg5petclinic/Spg5PetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.boleks.spg5petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spg5PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spg5PetClinicApplication.class, args);
+    }
+
+}
