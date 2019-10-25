9abfa6e2e6f3607c3c4bf394033ddfe3fc00f8c9
boleks <btrosic@gmail.com>
2019-10-25 17:59:23 +0200

    impl auto generated id

--- pet-clinic-web/src/main/java/com/boleks/spg5petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/boleks/spg5petclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Marko");
         owner1.setLastName("Nikolic");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Marija");
         owner2.setLastName("Savic");
 
@@ -38,13 +36,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Nikola");
         vet1.setLastName("Petrovic");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Boban");
         vet2.setLastName("Antonic");
 
