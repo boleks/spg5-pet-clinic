2b85adca5774e26c2e953b1a58f33ef6fbe9bf43
boleks <51618150+boleks@users.noreply.github.com>
2019-10-25 17:06:04 +0200

    add dataLoader

--- pet-clinic-data/src/main/java/com/boleks/spg5petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/boleks/spg5petclinic/services/map/OwnerServiceMap.java
@@ -1,11 +1,11 @@
 package com.boleks.spg5petclinic.services.map;
 
 import com.boleks.spg5petclinic.model.Owner;
-import com.boleks.spg5petclinic.services.CrudService;
+import com.boleks.spg5petclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
     public Set<Owner> findAll() {
@@ -32,4 +32,8 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
         super.deleteById(id);
     }
 
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
+    }
 }
