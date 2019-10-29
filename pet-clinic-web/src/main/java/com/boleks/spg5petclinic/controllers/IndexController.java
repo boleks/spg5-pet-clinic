abf57d8be94f3abb3baeadc49eb3e656b7928d2f
boleks <btrosic@gmail.com>
2019-10-29 18:21:18 +0100

    fix links

--- pet-clinic-web/src/main/java/com/boleks/spg5petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/boleks/spg5petclinic/controllers/IndexController.java
@@ -12,4 +12,10 @@ public class IndexController {
         return "index";
     }
 
+    @RequestMapping({"/find"})
+    public String oooups(){
+
+        return "notimplemented";
+    }
+
 }
--- pet-clinic-web/src/main/java/com/boleks/spg5petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/boleks/spg5petclinic/controllers/VetController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping({"/vets"})
+
 @Controller
 public class VetController {
 
@@ -15,11 +15,12 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @RequestMapping({"vets.html", "/vets", "/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
+
 }
