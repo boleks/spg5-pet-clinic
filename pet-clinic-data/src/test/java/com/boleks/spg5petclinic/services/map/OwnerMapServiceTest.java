98dc42df45b8596bc99f0032f963dd0ca7bae563
boleks <btrosic@gmail.com>
2019-11-05 13:36:44 +0100

    add tests

---
+++ pet-clinic-data/src/test/java/com/boleks/spg5petclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,80 @@
+package com.boleks.spg5petclinic.services.map;
+
+import com.boleks.spg5petclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+
+    final Long ownerId = 1l;
+    final String lastName = "Nikolic";
+
+    @BeforeEach
+    void setUp() {
+
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        ownerMapService.save(Owner.builder().id(ownerId).lastName(lastName).build());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownersSet = ownerMapService.findAll();
+        assertEquals(1, ownersSet.size());
+    }
+
+    @Test
+    void findById() {
+        Owner owner = ownerMapService.findById(ownerId);
+
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void saveExistingId() {
+        Long id = 2L;
+        Owner owner2 = Owner.builder().id(id).build();
+
+        Owner savedOwner = ownerMapService.save(owner2);
+
+        assertEquals(id, savedOwner.getId());
+
+    }
+
+    @Test
+    void saveNoId() {
+        Owner savedOwner = ownerMapService.save(Owner.builder().build());
+
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
+    }
+
+    @Test
+    void delete() {
+        ownerMapService.delete(ownerMapService.findById(ownerId));
+
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void deleteById() {
+
+        ownerMapService.deleteById(ownerId);
+
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void findByLastName() {
+
+        Owner nikolic = ownerMapService.findByLastName(lastName);
+
+        assertEquals(ownerId, nikolic.getId());
+
+    }
+}
